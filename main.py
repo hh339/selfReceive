83c674c62ef41884ebd6c9f39005a5972ca5851f
Yew Hui Hong <yewhuihong@U-HRY1WQ5X-2045.local>
2026-01-20 14:39:46 +0800

    add pay

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import base64
 import textwrap
 import json, time, os
 import sys
+import requests
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
@@ -102,7 +103,119 @@ def receive_data():
     #signature
     signature = sign("POST", "/receive", clientId, formattedDateTime, jsonResponse, privateKey)
 
-    print(signature)
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "response-time": formattedDateTime,
+        "client-id": clientId,
+        "signature": "algorithm=RSA256,keyVersion=1,signature=" + signature
+    }
+
+    # forward response as JSON
+    return Response(jsonResponse, 200, headers)
+
+@app.route('/getPaymentCode', methods=['POST'])
+def get_payment_code():
+    # Get the JSON data from the request
+    data = request.get_json()
+    region = data["region"]
+    customerId = data["customerId"]
+    codeQuantity = data["codeQuantity"]
+    terminalType = data["terminalType"]
+    osType = data["osType"]
+    deviceTokenId = data["deviceTokenId"]
+    clientIp = data["clientIp"]
+
+    jsonDict = {
+        "region": region,
+        "customerId": customerId,
+        "codeQuantity": codeQuantity,
+        "env":{
+            "terminalType": terminalType,
+            "osType": osType,
+            "deviceTokenId": deviceTokenId,
+            "clientIp": clientIp
+        },
+    }
+
+    jsonResponse = json.dumps(jsonDict)
+
+    #get value from env
+    load_dotenv()
+
+    #get value from env
+    clientId = os.getenv("CLIENTID")
+    privateKey = os.getenv("RSAENCRYPTKEY")
+
+    #get date time now
+    now = datetime.now(ZoneInfo("Asia/Kuala_Lumpur"))
+    formattedDateTime = now.strftime("%Y%m%d%H%M%S%z")
+
+    print(jsonResponse)
+
+    #signature
+    signature = sign("POST", "/aps/api/v1/codes/getPaymentCode", clientId, formattedDateTime, jsonResponse, privateKey)
+
+    #sending url
+    url = "https://open-sea-global.alipayplus.com/aps/api/v1/codes/getPaymentCode"
+
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "request-time": formattedDateTime,
+        "client-id": clientId,
+        "signature": "algorithm=RSA256,keyVersion=1,signature=" + signature
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        # data=jsonResponse,     # use data, not json
+        json=jsonDict,
+        timeout=10
+    )
+
+    print(response)
+
+    return Response(response, 200)
+
+@app.route('/pay', methods=['POST'])
+def pay():
+    # Get the JSON data from the request
+    data = request.get_json()
+    # paymentAmountValue = data["paymentAmount"]["value"]
+    # paymentAmountCurrency = data["paymentAmount"]["currency"]
+    # payToAmountValue = data["payToAmount"]["value"]
+    # payToAmountCurrency = data["payToAmount"]["currency"]
+    # paymentRequestId = data["paymentRequestId"]
+    print(data)
+
+    #get value from env
+    load_dotenv()
+
+    #get value from env
+    clientId = os.getenv("CLIENTID")
+    privateKey = os.getenv("RSAENCRYPTKEY")
+
+    #get value from env
+    clientId = os.getenv("CLIENTID")
+    privateKey = os.getenv("RSAENCRYPTKEY")
+
+    formattedDateTime = datetime.now(ZoneInfo("Asia/Kuala_Lumpur")).strftime("%Y%m%d%H%M%S%z")
+
+    jsonDict = {
+        "result":{
+            "resultCode": "SUCCESS",
+            "resultStatus": "S",
+            "resultMessage": "success"
+        },
+        "paymentId": datetime.now(ZoneInfo("Asia/Kuala_Lumpur")).strftime("%Y%m%d%H%M%S"),
+        "customerId": "1234567890123456",
+        "paymentTime": formattedDateTime
+    }
+
+    jsonResponse = json.dumps(jsonDict)
+
+    #signature
+    signature = sign("POST", "/receive", clientId, formattedDateTime, jsonResponse, privateKey)
 
     headers = {
         "Content-Type": "application/json; charset=UTF-8",
@@ -113,6 +226,7 @@ def receive_data():
 
     # forward response as JSON
     return Response(jsonResponse, 200, headers)
+
 
 if __name__ == "__main__":
     app.run(port=7777, debug=True)
